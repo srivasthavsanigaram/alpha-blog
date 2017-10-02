9f5f401a28c8d41529f32161a3f6bd5c4482414e
Srivasthav Sanigaram <srivasthavsanigaram@gmail.com>
2017-10-02 19:17:20 +0000

    New Test Application Which contains Homepage and About

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #get 'page/home', to: 'page#home'
+   root 'page#home'
+  #get 'page/about', to: 'page#about'
+   get 'about', to: 'page#about'
+end
