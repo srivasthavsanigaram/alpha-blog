d256882de4a04b667aef490be133462b785146ba
Srivasthav Sanigaram <srivasthavsanigaram@gmail.com>
2017-10-06 20:29:46 +0000

    Index and Delete Partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController 
+  
+  def index 
+   @articles = Article.all  
+  end  
+   
   def new 
    @article = Article.new
   end
-
+   
+   def edit 
+     @article = Article.find(params[:id])
+   end
+   
+   def update 
+    @article= Article.find(params[:id])
+    if @article.update(article_params)
+    flash[:notice] = "Article was successfully updated"
+    redirect_to article_path(@article)
+    else 
+     render 'edit'
+    end
+  end  
+   
   def create
     #render plain: params[:article].inspect 
     @article = Article.new(article_params)
@@ -18,6 +37,13 @@ class ArticlesController < ApplicationController
     @article=Article.find(params[:id])
   end 
   
+  def destroy
+   @article=Article.find(params[:id])
+   @article.destroy
+   flash[:notice] = "Article was successfully deleted" 
+   redirect_to articles_path
+  end 
+   
   private 
     def article_params
       params.require(:article).permit(:title, :description)
