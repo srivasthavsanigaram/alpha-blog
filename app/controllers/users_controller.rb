e4d3799c55960dc92ff9095b34d0f704e1612f3a
Srivasthav Sanigaram <srivasthavsanigaram@gmail.com>
2017-10-16 21:53:11 +0000

    Restrict UI articles, signup, sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :set_user, only:[:edit, :update, :show]
    def index 
     #@users = User.all
      @users = User.paginate(page: params[:page], per_page: 2) 
@@ -20,11 +21,10 @@ class UsersController < ApplicationController
    end
  
    def edit 
-     @user = User.find(params[:id])
+   
    end 
 
    def update
-     @user = User.find(params[:id])
      if @user.update(user_params)
        flash[:success] = "Your account was successfully updated"
        redirect_to articles_path
@@ -34,7 +34,6 @@ class UsersController < ApplicationController
    end 
    
    def show 
-     @user = User.find(params[:id])
      @user_articles=@user.articles.paginate(page:params[:page], per_page: 2)
    end 
    
@@ -42,5 +41,9 @@ class UsersController < ApplicationController
    private 
    def user_params
      params.require(:user).permit(:username, :email, :password)
+   end
+  
+   def set_user
+     @user = User.find(params[:id])
    end
 end 
