514311b76a1c531ec75c1cad43e97b7f3b59be0a
Srivasthav Sanigaram <srivasthavsanigaram@gmail.com>
2017-10-17 20:52:08 +0000

    signup user interface shows after signing up it shows nothing stauts in login position fixed under user_controller def create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,10 @@ class UsersController < ApplicationController
      #debugger
      @user = User.new(user_params)
      if @user.save
+       session[:user_id]=@user.id
        flash[:success] = "Welcome to Atom Tech Group #{@user.username}"
-       redirect_to articles_path
+       #redirect_to articles_path
+       redirect_to user_path(@user)
      else
         render 'new'
      end 
