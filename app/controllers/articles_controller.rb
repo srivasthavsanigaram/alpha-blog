066fe66508e49cc0a135210f64d41fad11e8dfc4
Srivasthav Sanigaram <srivasthavsanigaram@gmail.com>
2017-10-17 20:13:06 +0000

    Completed Restrict on Users

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController 
   before_action :set_article, only: [:edit, :update, :show, :destroy] 
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy] 
    
   def index 
    #@articles = Article.all  
@@ -26,7 +28,8 @@ class ArticlesController < ApplicationController
     debugger
     #render plain: params[:article].inspect 
     @article = Article.new(article_params)
-    @article.user = User.first
+    #@article.user = User.first
+    @article.user = current_user
     if @article.save
     flash[:success]="Article was successfully created"
     redirect_to article_path(@article)
@@ -48,7 +51,16 @@ class ArticlesController < ApplicationController
     def set_article
      @article=Article.find(params[:id])
    end  
-    def article_params
+   
+   def article_params
       params.require(:article).permit(:title, :description)
-    end
+   end
+   
+   def require_same_user
+     if current_user != @article.user
+     flash[:danger] = "you can only edit or delete your own articles"
+     redirect_to root_path
+     end 
+   end
 end
+
