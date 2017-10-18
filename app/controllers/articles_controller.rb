7a5b2c26c1000e5479041169ff21cf15b5759d11
Srivasthav Sanigaram <srivasthavsanigaram@gmail.com>
2017-10-18 16:59:22 +0000

    debugger commented under articles controller file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,7 +25,7 @@ class ArticlesController < ApplicationController
   end  
    
   def create
-    debugger
+    #debugger
     #render plain: params[:article].inspect 
     @article = Article.new(article_params)
     #@article.user = User.first
